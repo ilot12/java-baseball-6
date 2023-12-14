fd00980ae8a3fbfbc77d71de77f8fbefce21281a
ilot12 <rlaehdwn60@gmail.com>
2023-12-14 15:35:11 +0900

    feat: Application에 게임 시작 추가
    do-while문으로 게임을 반복하도록 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,14 @@
 package baseball;
 
+import game.Game;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        
+        Game game;
+        do {
+            game = new Game();
+        }while(game.getRepeatGame());
+        
     }
 }
